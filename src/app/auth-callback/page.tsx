0a0a7d3bf147e8200a848030546046f480657982
변찬우 <byun00106@gmail.com>
2023-10-06 12:44:38 +0900

    feat: add prisma

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,8 +1,10 @@
-import { useSearchParams } from 'next/navigation'
-import { useRouter } from 'next/router'
-import { trpc } from '../_trpc/client'
+'use client'
 
-const Page = async () => {
+import { useSearchParams, useRouter } from 'next/navigation'
+import { trpc } from '../_trpc/client'
+import { Loader2 } from 'lucide-react'
+
+const Page = () => {
   const router = useRouter()
 
   const searchParams = useSearchParams()
@@ -14,7 +16,24 @@ const Page = async () => {
         router.push(origin ? `/${origin}` : '/dashboard')
       }
     },
+    onError: (error) => {
+      if (error.data?.code === 'UNAUTHORIZED') {
+        router.push('/sign-in')
+      }
+    },
+    retry: true,
+    retryDelay: 500,
   })
+
+  return (
+    <div className='w-full mt-24 flex justify-center'>
+      <div className='flex flex-col items-center gap-2'>
+        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+        <h3 className='font-semibold text-xl'>Setting up your account...</h3>
+        <p>You will be redirected automatically</p>
+      </div>
+    </div>
+  )
 }
 
 export default Page
