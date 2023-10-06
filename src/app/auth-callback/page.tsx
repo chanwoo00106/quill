39c34c3fe601b729943f52a6be3572204da578bf
chanwoo00106 <byun00106@gmail.com>
2023-10-06 11:37:40 +0900

    feat: add trpc setup

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,20 @@
+import { useSearchParams } from 'next/navigation'
+import { useRouter } from 'next/router'
+import { trpc } from '../_trpc/client'
+
+const Page = async () => {
+  const router = useRouter()
+
+  const searchParams = useSearchParams()
+  const origin = searchParams.get('origin')
+
+  const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+    onSuccess: ({ success }) => {
+      if (success) {
+        router.push(origin ? `/${origin}` : '/dashboard')
+      }
+    },
+  })
+}
+
+export default Page
