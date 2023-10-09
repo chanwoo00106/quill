c1206b28cad04784a6571d9c880b5bba1712e5ca
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-10-09 23:13:15 +0900

    feat: add loading

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -37,6 +37,9 @@ const PdfRenderer = ({ url }: Props) => {
   const [currPage, setCurrPage] = useState<number>(1)
   const [scale, setScale] = useState<number>(1)
   const [rotation, setRotation] = useState<number>(0)
+  const [renderedScale, setRenderedScale] = useState<number | null>(null)
+
+  const isLoading = renderedScale !== scale
 
   const CustomPageValidator = z.object({
     page: z
@@ -171,11 +174,29 @@ const PdfRenderer = ({ url }: Props) => {
               file={url}
               className='max-h-full'
             >
+              {isLoading && renderedScale ? (
+                <Page
+                  width={width ? width : 1}
+                  pageNumber={currPage}
+                  scale={scale}
+                  rotate={rotation}
+                  key={'@' + renderedScale}
+                />
+              ) : null}
+
               <Page
+                className={cn(isLoading ? 'hidden' : '')}
                 width={width ? width : 1}
                 pageNumber={currPage}
                 scale={scale}
                 rotate={rotation}
+                key={'@' + scale}
+                loading={
+                  <div className='flex justify-center'>
+                    <Loader2 className='my-24 h-6 w-6 animate-spin' />
+                  </div>
+                }
+                onRenderSuccess={() => setRenderedScale(scale)}
               />
             </Document>
           </div>
